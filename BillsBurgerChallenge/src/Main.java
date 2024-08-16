ad4e1e084d808d2db18ed444cddd749525cb932e
hristo-W3 <hristo_valeriev@yahoo.com>
2024-08-16 19:22:44 +0300

    Burger + Toppings added

--- BillsBurgerChallenge/src/Main.java
+++ BillsBurgerChallenge/src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        Item coke = new Item("drink", "coke", 1.50);
-        coke.printItem();
-        coke.setSize("LARGE");
-        coke.printItem();
+//        Item coke = new Item("drink", "coke", 1.50);
+//        coke.printItem();
+//        coke.setSize("LARGE");
+//        coke.printItem();
+//
+//        Item avocado = new Item("Topping", "avocado", 1.50);
+//        avocado.printItem();
 
-        Item avocado = new Item("Topping", "avocado", 1.50);
-        avocado.printItem();
+        Burger burger = new Burger("regular", 5.00);
+        burger.addToppings("BACON","CHEESE","MAYO");
+        burger.printItem();
     }
 }
