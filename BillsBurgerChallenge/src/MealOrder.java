80f441d48605473a335b59f65c0362872ed7c24e
hristo-W3 <hristo_valeriev@yahoo.com>
2024-08-16 19:57:05 +0300

    Update MealOrder.java

--- BillsBurgerChallenge/src/MealOrder.java
+++ BillsBurgerChallenge/src/MealOrder.java
@@ -41,6 +41,14 @@ public class MealOrder {
         burger.addToppings(extra1,extra2,extra3);
     }
 
+    public void addBurgerToppings(String extra1, String extra2, String extra3, String extra4, String extra5) {
+        if (burger instanceof DeluxeBurger db) {
+            db.addToppings(extra1, extra2, extra3, extra4, extra5);
+        } else {
+            burger.addToppings(extra1,extra2,extra3);
+        }
+    }
+
     public void setDrinkSize(String size) {
         drink.setSize(size);
     }
