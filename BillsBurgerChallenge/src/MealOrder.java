de2d05d1b03532bf8c91efe1a548cc7def537721
hristo-W3 <hristo_valeriev@yahoo.com>
2024-08-16 19:55:00 +0300

    Update MealOrder.java

--- BillsBurgerChallenge/src/MealOrder.java
+++ BillsBurgerChallenge/src/MealOrder.java
@@ -8,19 +8,31 @@ public class MealOrder {
     }
 
     public MealOrder(String burgerType, String drinkType, String sideType) {
-        this.burger = new Burger(burgerType, 4.00);
+        if (burgerType.equalsIgnoreCase("deluxe")) {
+            this.burger = new DeluxeBurger(burgerType, 8.50);
+        } else {
+            this.burger = new Burger(burgerType, 4.00);
+        }
         this.drink = new Item("drink", drinkType, 1.00);
         this.side = new Item("side", sideType, 1.50);
     }
 
     public double getTotalPrice() {
+        if (burger instanceof DeluxeBurger) {
+            return burger.getAdjustedPrice();
+        }
         return burger.getAdjustedPrice() + drink.getAdjustedPrice() + side.getAdjustedPrice();
     }
 
     public void printItemizedList() {
         burger.printItem();
-        drink.printItem();
-        side.printItem();
+        if (burger instanceof DeluxeBurger) {
+            Item.printItem(drink.getName(),0);
+            Item.printItem(side.getName(),0);
+        } else {
+            drink.printItem();
+            side.printItem();
+        }
         System.out.println("-".repeat(30));
         Item.printItem("TOTAL PRICE", getTotalPrice());
     }
