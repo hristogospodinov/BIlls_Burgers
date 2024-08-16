1c4dbda2317e8f955974f5aeee9bf7b0bef3b057
hristo-W3 <hristo_valeriev@yahoo.com>
2024-08-16 19:38:32 +0300

    Finalizing MealOrder and tests

--- BillsBurgerChallenge/src/Main.java
+++ BillsBurgerChallenge/src/Main.java
@@ -9,8 +9,22 @@ public class Main {
 //        Item avocado = new Item("Topping", "avocado", 1.50);
 //        avocado.printItem();
 
-        Burger burger = new Burger("regular", 5.00);
-        burger.addToppings("BACON","CHEESE","MAYO");
-        burger.printItem();
+//        Burger burger = new Burger("regular", 5.00);
+//        burger.addToppings("BACON","CHEESE","MAYO");
+//        burger.printItem();
+
+//        MealOrder meal = new MealOrder();
+//        meal.printItemizedList();
+
+//        MealOrder regularMeal = new MealOrder();
+//        regularMeal.addBurgerToppings("BACON","CHEESE","MAYO");
+//        regularMeal.setDrinkSize("LARGE");
+//        regularMeal.setSideSize("SMALL");
+//        regularMeal.printItemizedList();
+
+        MealOrder meal2 = new MealOrder("turkey", "7-up", "chili");
+        meal2.addBurgerToppings("LETTUCE","TOMATO","KETCHUP");
+        meal2.setDrinkSize("SMALL");
+        meal2.printItemizedList();
     }
 }
