661629fa956748466b13e1ad23763276867e6c2f
hristo-W3 <hristo_valeriev@yahoo.com>
2024-08-16 17:33:57 +0300

    Update Item.java

--- BillsBurgerChallenge/src/Item.java
+++ BillsBurgerChallenge/src/Item.java
@@ -1,2 +1,20 @@
 public class Item {
+    private String type;
+    private String name;
+    private double price;
+    private String size = "MEDIUM";
+
+    public Item(String type, String name, double price) {
+        this.type = type.toUpperCase();
+        this.name = name.toUpperCase();
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBasePrice() {
+        return price;
+    }
 }
